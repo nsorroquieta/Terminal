d84c055c576781f8bf50bf42e515d70a4cf0804a
Goday Soria, José Leonel <jgodaysoria@antel.com.uy>
2018-10-18 19:03:43 -0300

    commit Message

---
+++ primary-source/src/main/java/uy/com/antel/SocketClient.java
@@ -0,0 +1,33 @@
+package uy.com.antel;
+
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.Socket;
+
+public class SocketClient {
+
+    static final String Host = "localhost";
+    static final int port = 9898;
+    static Socket sc;
+    static DataOutputStream mensaje;
+    static DataInputStream entrada;
+
+
+    public static void main(String[] args){
+
+        try {
+            sc = new Socket(Host, port);
+            mensaje = new DataOutputStream(sc.getOutputStream());
+            mensaje.writeUTF("Hola que tal");
+
+            sc.close();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+
+}
