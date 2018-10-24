be3452a3a459c8cf6e52116657c777c937a1c366
Nico <nicolas.sorroquieta@gmail.com>
2018-10-24 14:06:14 -0300

    Envia los datos necesarios para crear el ticket

--- primary-source/src/main/java/uy/com/antel/Terminal.java
+++ primary-source/src/main/java/uy/com/antel/Terminal.java
@@ -1,10 +1,15 @@
 package uy.com.antel;
 
+import oracle.jrockit.jfr.parser.ParseException;
+
 import java.io.BufferedReader;
 import java.io.DataOutputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.Socket;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class Terminal {
 
@@ -14,6 +19,7 @@ public class Terminal {
 
         do {
             SocketClient sc = new SocketClient();
+
             System.out.println("");
             System.out.println("Menu Principal");
             System.out.println("------------------");
@@ -21,7 +27,7 @@ public class Terminal {
             System.out.println("2) Anular Ticket -> A");
             System.out.println("3) Ayuda -> H");
             System.out.println("----------------");
-            System.out.print("Ingrese el comando->");
+            System.out.print("Ingrese el comando ->");
             String linea = null;
             try {
                 linea = userInput.readLine();
@@ -31,7 +37,6 @@ public class Terminal {
             String[] result = linea.split("\\s");
 
             String inputCommand = result[0].toLowerCase();
-            System.out.println(inputCommand.hashCode());
             switch (inputCommand.hashCode()){
                 case 104: // H
                     System.out.println("");
@@ -55,49 +60,55 @@ public class Terminal {
                             Boolean flagStartTime = true;
                             Boolean flagMinutes = true;
                             do {
-                                System.out.print("Matricula->");
+                                System.out.print("Matricula ->");
                                 carRegistration = userInput.readLine();
                                 if (carRegistration.trim().equals("")) {
                                     System.out.println("----------------");
                                     System.out.println("Matricula Inválida.");
                                     System.out.println("----------------");
-                                    flagCarRegistration = true;
                                 } else {
                                     flagCarRegistration = false;
                                 }
                             } while (flagCarRegistration);
 
                             do {
-                                System.out.print("Fecha de inicio->");
+                                System.out.print("Fecha de inicio <dd-MM-yyyy HH:mm:ss> ->");
 
                                 startDate = userInput.readLine();
+
                                 if (startDate.trim().equals("")) {
                                     System.out.println("----------------");
                                     System.out.println("Fecha inválida.");
                                     System.out.println("----------------");
-                                    flagStartTime = true;
                                 } else {
-                                    flagStartTime = false;
+                                    SimpleDateFormat formatter = new SimpleDateFormat("dd-MM-yyyy HH:mm:ss");
+                                    try {
+                                        Date date = formatter.parse(startDate);
+                                        flagStartTime = false;
+                                    } catch (java.text.ParseException e) {
+                                        System.out.println("----------------");
+                                        System.out.println("La fecha debe tener el siguiente formato dd-MM-yyyy HH:mm:ss");
+                                        System.out.println("----------------");
+                                    }
                                 }
                             } while (flagStartTime);
 
                             do {
-                                System.out.print("Cantidad de minutos (15-30-45-60-90) ->");
+                                System.out.print("Cantidad de minutos <15-30-45-60-90> ->");
                                 minutes = userInput.readLine();
                                 if (minutes.trim().equals("15") || minutes.trim().equals("30") || minutes.trim().equals("45") || minutes.trim().equals("60") || minutes.trim().equals("90") || minutes.trim().equals("120")) {
                                     flagMinutes = false;
                                 } else {
                                     System.out.println("----------------");
-                                    System.out.println("Ingrese un valor válido.");
+                                    System.out.println("Cantidad de minutos inválida, por favor ingrese un valor válido.");
                                     System.out.println("----------------");
-                                    flagMinutes = true;
                                 }
                             } while (flagMinutes);
                             buyFlag = false;
                         } while (buyFlag);
 
                         String json = "{\"command\":\"buyTicket\",\"carRegistration\":\"" + carRegistration + "\",\"startDate\":\"" + startDate + "\",\"minutes\":\""+minutes+"\"}";
-                        //System.out.println(json);
+                        System.out.println(json);
         //c                System.out.println(sc.reciveMessage());
                         String response = sc.sendMessage(json);
                         System.out.println("Agencia: "+response);
